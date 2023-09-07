43108c549f0129b8b18f6dff97eba7a4443038a3
nusratullo_a <axunovnusrat@gmail.com>
2023-09-07 22:17:27 +0500

    fsggs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
